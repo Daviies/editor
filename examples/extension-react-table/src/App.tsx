784a04576194ca412dd97e196b3df869b87e17cb
Daviies <106864737+Daviies@users.noreply.github.com>
2025-11-04 21:55:59 +0300

    Update App.tsx

--- examples/extension-react-table/src/App.tsx
+++ examples/extension-react-table/src/App.tsx
@@ -1,10 +1,4 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
+
 import {AutoFocusExtension} from '@lexical/extension';
 import {HistoryExtension} from '@lexical/history';
 import {ExtensionComponent} from '@lexical/react/ExtensionComponent';
